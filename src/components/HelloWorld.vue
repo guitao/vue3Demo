4589e4f10b90243ec9df194a84a57a26d99e65ac
guitao <38004944+guitao@users.noreply.github.com>
2021-05-11 22:23:05 +0800

    first add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <h2>Filter LearnVue Articles</h2>
+  <input type="text" placeholder="Filter Search" v-model="query" />
+  <button @click="reset">clear</button>
+
+  <search-results :query='query'/>
+</template>
+
+<script>
+import { ref } from "vue";
+import SearchResults from './SearchResults.vue'
+
+export default {
+  name: "HelloWorld",
+  components: {
+    SearchResults
+  },
+  props: {
+    msg: String,
+  },
+  setup() {
+    const query = ref("");
+
+    const reset = (e) => {
+      query.value = ''
+    }
+
+    return {
+      query,
+      reset
+    };
+  },
+};
+</script>
